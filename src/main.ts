354ca5920a5457406e3ea4a79728dc79ac9fc1ec
Alexander Repin <rc@kode.ru>
2021-09-24 18:25:21 +0200

    Auto callable bootstrap() function

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   await app.listen(3000);
-}
-bootstrap();
+})();
